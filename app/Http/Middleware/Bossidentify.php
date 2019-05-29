3c1f3b4b65629490bbe2b41fbb25b5b2aa9f86ac
JodyYan <stu891272004@yahoo.com.tw>
2019-05-29 10:59:33 +0800

    set bossidentify middleware

---
+++ app/Http/Middleware/Bossidentify.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Boss;
+use Closure;
+
+class Bossidentify
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token=$request->bearerToken();
+        if ($token==null) {
+            return response(['error'=>'without token'], 401);
+        }
+        if (!Boss::where('api_token', $token)->exists()) {
+            return response(['error'=>'token dose not exist'], 401);
+        }
+        return $next($request);
+    }
+}
