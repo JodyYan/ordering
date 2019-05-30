9eefac89d3d8255bc8bf8cf60fbf60d49a51ae1d
JodyYan <stu891272004@yahoo.com.tw>
2019-05-30 11:16:11 +0800

    create member tables

---
+++ database/migrations/2019_05_30_110319_create_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('account')->unique();
+            $table->string('password');
+            $table->string('api_token', 60)->unique()->nullable();
+            $table->integer('rice')->default(1);
+            $table->integer('vegetable')->default(1);
+            $table->bigInteger('group_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
