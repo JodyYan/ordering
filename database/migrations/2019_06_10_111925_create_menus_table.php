79a9424daac7fd76ddeda7ad7f542d2680cd612e
JodyYan <stu891272004@yahoo.com.tw>
2019-06-10 13:44:21 +0800

    create menu tables

---
+++ database/migrations/2019_06_10_111925_create_menus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('price');
+            $table->bigInteger('group_id')->nullable()->unsigned();
+            $table->integer('quantity_limit')->nullable();
+            $table->string('note')->nullable();
+            $table->date('menu_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
