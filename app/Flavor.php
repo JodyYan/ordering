b64fc99826d64c4d998536c86e19363b1ef88ed3
JodyYan <stu891272004@yahoo.com.tw>
2019-06-11 16:22:09 +0800

    set relationship between menus and flavors

--- app/Flavor.php
+++ app/Flavor.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Flavor extends Model
 {
     protected $guarded=[];
+    public function menu()
+    {
+        return $this->belongTo('App\Menu');
+    }
 }
--- app/Menu.php
+++ app/Menu.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     protected $guarded=[];
+
+    public function flavors()
+    {
+        return $this->hasMany('App\Flavor');
+    }
 }
