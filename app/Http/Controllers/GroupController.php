0c67646d32c74bd64c088c4b2c87d0c99becb0b5
JodyYan <stu891272004@yahoo.com.tw>
2019-06-17 11:18:07 +0800

    set time_limit and preset_time in group function store

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -11,7 +11,9 @@ class GroupController extends Controller
     public function store()
     {
         $group=Group::create(request()->validate([
-            'name'=>['required', 'string', 'unique:groups,name']
+            'name'=>['required', 'string', 'unique:groups,name'],
+            'time_limit' => ['boolean'],
+            'preset_time' => ['date_format:H:i', 'required_if:time_limit,1']
         ]));
         return $group;
     }
