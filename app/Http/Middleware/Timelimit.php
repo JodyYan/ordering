8b9ac579c53c968bc98506021c56cd0661960f3a
JodyYan <stu891272004@yahoo.com.tw>
2019-06-23 13:58:27 +0800

    update the time limit rule of Timelimit middleware

--- app/Http/Middleware/Timelimit.php
+++ app/Http/Middleware/Timelimit.php
@@ -6,6 +6,7 @@ use Closure;
 use App\Deadline;
 use App\Member;
 use App\Group;
+use App\Menu;
 use Carbon\Carbon;
 
 class Timelimit
@@ -22,21 +23,26 @@ class Timelimit
         $token=$request->bearerToken();
         $member=Member::where('api_token', $token)->first();
         $today=Carbon::today();
+        $tomorrow=Carbon::tomorrow();
         $nowTime=Carbon::now();
         $timeCheck=Deadline::where('group_id', $member->group_id)->where('which_date', $today)->first();
-        if ($member->group->time_limit == 1) {
-            if ($timeCheck != null) {
-                $timeCarbon=Carbon::parse($timeCheck->deadline_time);
-                if ($nowTime->gt($timeCarbon)) {
+        $menu=Menu::find($request->get('menu_id'));
+        $menuDate=Carbon::parse($menu->menu_date);
+
+        if ($menuDate->eq($tomorrow)) {
+            if ($member->group->time_limit == 1) {
+                if ($timeCheck != null) {
+                    $timeCarbon=Carbon::parse($timeCheck->deadline_time);
+                    if ($nowTime->gt($timeCarbon)) {
+                        return response(['error' => 'over order time'], 422);
+                    }
+                }
+
+                if ($timeCheck == null && Carbon::parse($member->group->preset_time)->lt($nowTime)) {
                     return response(['error' => 'over order time'], 422);
                 }
             }
-
-            if ($timeCheck == null && Carbon::parse($member->group->preset_time)->lt($nowTime)) {
-                return response(['error' => 'over order time'], 422);
-            }
         }
-
         return $next($request);
     }
 }
