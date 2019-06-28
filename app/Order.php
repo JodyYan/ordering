e9e19eaa44dd426a83cd4fcf1993c21930db5954
JodyYan <stu891272004@yahoo.com.tw>
2019-06-28 16:41:11 +0800

    set relationship of order belongsTo member

--- app/Order.php
+++ app/Order.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $guarded=[];
+    public function member()
+    {
+        return $this->belongsTo('App\Member', 'user_id');
+    }
 }
